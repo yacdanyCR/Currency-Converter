7207619945d32733f1106696271d993041b91d48
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-07 10:17:09 -0600

    Creating logic to fecth from an api

--- src/Components/DropdownComponent/DropdownComponent.jsx
+++ src/Components/DropdownComponent/DropdownComponent.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
-import InputGroupComponent from '../InputGroupComponent/InputGroupComponent'
+import React, { useState } from 'react'
+import { Button, InputGroup, Form } from 'react-bootstrap'
+import { getCurrencyConversion } from '../../Services/get-currencyConverter';
 import './style.css'
 
 export const DropdownComponent = () => {
+    const [from, setFrom] = useState("");
+    const [to, setTo] = useState("");
+    const [mount, setMount] = useState(0);
+    const [conversion, setConversion] = useState(0);
+
+    const handleClick = async () => {
+        const result = await getCurrencyConversion(from, to, mount)
+        setConversion(result)
+    }
+
     return (
         <main>
             <section className='Currency_converter'>
                 <div className='container'>
-                    <InputGroupComponent title="From" />
-                    <Button variant="outline-warning">Convert</Button>
-                    <InputGroupComponent />
+                    <InputGroup className="mb-3">
+                        <Form.Select title='From' onChange={(e) => setFrom(e.target.value)}>
+                            <option>From</option>
+                            <option value={"CRC"}>CRC</option>
+                        </Form.Select>
+                        <Form.Control aria-label="Number input" type='number' onChange={(e) => setMount(e.target.value)} />
+                    </InputGroup>
+
+                    <Button variant="outline-warning" onClick={() => handleClick()}>Convert</Button>
+
+                    <InputGroup className="mb-3">
+                        <Form.Select onChange={(e) => setTo(e.target.value)}>
+                            <option>To</option>
+                            <option value={"USD"}>USD</option>
+                        </Form.Select>
+                        <Form.Control aria-label="Number input" type='number' value={conversion} disabled />
+                    </InputGroup>
                 </div>
             </section>
         </main >
