c9937955aa6fbd6d7eb64387e201be1910d6cf91
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-07 08:36:42 -0600

    Adding img logo

---
+++ src/Components/LogoComponent/LogoComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+export const LogoComponent = () => {
+    return (
+        <section>
+            <div className="logo_Section">
+                <div className="container">
+                    <img src="logo.png" alt="logo" />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default LogoComponent
